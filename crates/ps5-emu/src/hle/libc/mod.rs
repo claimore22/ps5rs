f03f9d4203c4a2bd35b085ca8bf4233edbc0b646
claimore22 <claimore22@gmail.com>
2026-09-07 02:08:11 -0400

    restore rand handler and remove qsort from libc

--- crates/ps5-emu/src/hle/libc/mod.rs
+++ crates/ps5-emu/src/hle/libc/mod.rs
@@ -55,7 +55,7 @@ impl HleModule for LibcModule {
             HostCall::CatchReturnFromMain => calls::catch_return_from_main(args),
             HostCall::Puts => calls::puts(host, args),
             HostCall::Printf => calls::printf(host, args),
-            HostCall::Qsort => calls::qsort(host, args),
+            HostCall::Rand => calls::rand(&mut ctx.libc),
             _ => Err(EmuError::NoHandler("libc".to_string())),
         }
     }
